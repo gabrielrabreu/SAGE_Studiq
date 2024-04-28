53f7a950005b586381af3c58e39791c430b00bb0
Gabriel Abreu <gabrielabreu.development@gmail.com>
2024-04-28 11:16:53 -0300

    Initial

---
+++ web/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,18 @@
+const HomePage: React.FC = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <div>
+        <h1
+          className="
+            font-semibold py-4
+            text-black
+            dark:text-white"
+        >
+          Home
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
